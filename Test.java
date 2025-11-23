d3eca4f8149ca2b4a1f56555b7722f25655814b3
Nilesh Bhurewar <137190915+nileshbhurewar@users.noreply.github.com>
2025-11-23 11:50:44 +0530

    Fix syntax errors in Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 class Test{
 public static void main(String[]args){
-String name = "nilesh"
-String name1 = "nilesh1"
+String name = "nilesh";
+String name1 = "nilesh1";
   System.out.println(name);
   System.out.println(name1);
 }
